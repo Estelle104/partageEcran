869681f5170e228f725c490f6128b89d5913cc60
AndryKT <andryrandria528@gmail.com>
2026-01-27 15:09:34 +0300

    appl

---
+++ server/input_apply.py
@@ -0,0 +1,37 @@
+from pynput.mouse import Controller as MouseController, Button
+from pynput.keyboard import Controller as KeyboardController, Key
+import json
+
+mouse = MouseController()
+keyboard = KeyboardController()
+
+class InputApply:
+
+    def handle(self, data):
+        event = json.loads(data)
+
+        if event["type"] == "mouse_move":
+            mouse.position = (event["x"], event["y"])
+
+        elif event["type"] == "mouse_click":
+            btn = Button.left if event["button"] == "left" else Button.right
+            if event["pressed"]:
+                mouse.press(btn)
+            else:
+                mouse.release(btn)
+
+        elif event["type"] == "mouse_scroll":
+            mouse.scroll(event["dx"], event["dy"])
+
+        elif event["type"] == "key_press":
+            key = self.parse_key(event["key"])
+            keyboard.press(key)
+
+        elif event["type"] == "key_release":
+            key = self.parse_key(event["key"])
+            keyboard.release(key)
+
+    def parse_key(self, key):
+        if key.startswith("Key."):
+            return getattr(Key, key.split(".")[1])
+        return key.strip("'")
--- server/server.py
+++ server/server.py
@@ -1,47 +1,62 @@
 import socket
 import struct
+import threading
 
 from server.screen_capture import get_frame
 from server.encoder import encode_frame
 from config.settings import SERVER_IP, SERVER_PORT
+from server.input_apply import InputApply
+
+
+def input_loop(client_socket):
+    while True:
+        size_data = client_socket.recv(4)
+        if not size_data:
+            break
+
+        size = struct.unpack(">I", size_data)[0]
+        data = client_socket.recv(size)
+        input_apply.handle(data)
+
+
+def screen_loop(client_socket):
+    while True:
+        frame = get_frame()
+        if frame is None:
+            continue
+
+        data = encode_frame(frame)
+        if data is None:
+            continue
+
+        client_socket.sendall(struct.pack(">I", len(data)))
+        client_socket.sendall(data)
+
 
 def start_server():
-    # 1. Création du socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    # 2. Bind UNE SEULE FOIS
     server_socket.bind((SERVER_IP, SERVER_PORT))
+    server_socket.listen(1)
 
-    # 3. Listen UNE SEULE FOIS
-    server_socket.listen(2)
     print(f"[SERVER] En attente de connexion sur {SERVER_IP}:{SERVER_PORT}")
-
-    # 4. Accept UNE SEULE FOIS
     client_socket, addr = server_socket.accept()
     print(f"[SERVER] Client connecté depuis {addr}")
 
     try:
-        while True:
-            frame = get_frame()
-            if frame is None:
-                continue
+        t1 = threading.Thread(target=input_loop, args=(client_socket,), daemon=True)
+        t2 = threading.Thread(target=screen_loop, args=(client_socket,), daemon=True)
 
-            data = encode_frame(frame)
-            if data is None:
-                continue
+        t1.start()
+        t2.start()
 
-            # Envoi taille + données
-            client_socket.sendall(struct.pack(">I", len(data)))
-            client_socket.sendall(data)
-
-    except Exception as e:
-        print("[SERVER] Erreur :", e)
+        t1.join()
+        t2.join()
 
     finally:
         client_socket.close()
         server_socket.close()
-        print("[SERVER] Serveur arrêté")
+
 
 if __name__ == "__main__":
     start_server()

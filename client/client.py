44efcbadb2bd4c97aa1d5efc936289852de0557b
AndryKT <andryrandria528@gmail.com>
2026-01-27 15:04:14 +0300

    Client

--- client/client.py
+++ client/client.py
@@ -2,10 +2,14 @@ from config.settings import SERVER_IP, SERVER_PORT
 from client.network import connect_to_server, receive_frame
 from client.decoder import decode_frame
 from client.display import show_frame
+from client.input_capture import InputCapture
 
 
 def main():
     sock = connect_to_server(SERVER_IP, SERVER_PORT)
+
+    input_capture = InputCapture(sock)
+    input_capture.start()
 
     while True:
         frame_data = receive_frame(sock)
